ec8da39373ecaf3d389013a2518ba5588a928111
noman <nomanmd1st2001@gmail.com>
2022-09-29 18:23:26 +0600

    add onteher questioin

--- src/component/Main/Question/Question.js
+++ src/component/Main/Question/Question.js
@@ -6,9 +6,9 @@ const Question = () => {
             <dl>
                 <dt>how does react work</dt>
                 <dd>React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible.</dd>
-                <dt></dt>
-                <dd></dd>
-                <dt></dt>
+                <dt>What is the difference between props and state?</dt>
+                <dd>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</dd>
+                <dt>What is Useeffect used for?</dt>
                 <dd></dd>
             </dl>
         </div>
